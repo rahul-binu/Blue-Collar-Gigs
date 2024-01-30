503d7c297caf7fe24818110df58113d057687aee
rahul-binu <just.commit.it@gmail.com>
2024-01-30 20:45:00 +0530

    added all needy folders like repo entity ect..

--- bcg-backend/src/main/java/net/bluecollargigs/bcgbackend/controllers/TestController.java
+++ bcg-backend/src/main/java/net/bluecollargigs/bcgbackend/controllers/TestController.java
@@ -1,5 +1,6 @@
 package net.bluecollargigs.bcgbackend.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,11 @@ public class TestController {
     @RequestMapping("/hello")
     public String hello(){
         return "Hello World !";
+    }
+
+    @GetMapping("/GetMapping")
+    public String Mapping(){
+        return "Get  Mapping";
     }
     
 }

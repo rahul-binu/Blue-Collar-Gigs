5982e2b8363e974ac57e808546ea47fecb197916
rahul-binu <just.commit.it@gmail.com>
2024-01-31 18:45:21 +0530

    compleated the userRepository

--- bcg-backend/src/main/java/net/bluecollargigs/bcgbackend/repository/UserRepository.java
+++ bcg-backend/src/main/java/net/bluecollargigs/bcgbackend/repository/UserRepository.java
@@ -1,5 +1,11 @@
 package net.bluecollargigs.bcgbackend.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import net.bluecollargigs.bcgbackend.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
     
 }

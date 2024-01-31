98d6abeb0a6bf745db795953669ac8ab18fc85a7
rahul-binu <just.commit.it@gmail.com>
2024-01-31 18:52:07 +0530

    compleated the userMapper

--- bcg-backend/src/main/java/net/bluecollargigs/bcgbackend/mapper/UserMapper.java
+++ bcg-backend/src/main/java/net/bluecollargigs/bcgbackend/mapper/UserMapper.java
@@ -1,5 +1,26 @@
 package net.bluecollargigs.bcgbackend.mapper;
 
+import net.bluecollargigs.bcgbackend.dto.UserDto;
+import net.bluecollargigs.bcgbackend.entity.User;
+
 public class UserMapper {
-    
+    public static UserDto mapToUserDto(User user){
+        return new UserDto(
+            user.getId(),
+            user.getEmail(),
+            user.getPassword(),
+            user.getUserType(),
+            user.getRegistrationDate()
+        );
+    }
+
+    public static User mapToUser(UserDto userDto){
+        return new User(
+            userDto.getId(),
+            userDto.getEmail(),
+            userDto.getPassword(),
+            userDto.getUserType(),
+            userDto.getRegistrationDate()
+        );
+    }
 }

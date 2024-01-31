eeed7276b5e9fd646a06535ebefce3746ff0a158
rahul-binu <just.commit.it@gmail.com>
2024-01-31 19:09:30 +0530

    Added the createUser REST API and saving the UserService

--- bcg-backend/src/main/java/net/bluecollargigs/bcgbackend/controllers/UserController.java
+++ bcg-backend/src/main/java/net/bluecollargigs/bcgbackend/controllers/UserController.java
@@ -1,5 +1,28 @@
 package net.bluecollargigs.bcgbackend.controllers;
 
+import net.bluecollargigs.bcgbackend.dto.UserDto;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+import net.bluecollargigs.bcgbackend.service.UserService;
+
+@CrossOrigin("*")
 public class UserController {
-    
+    private UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    // createUser REST API
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
+        UserDto savedUser = userService.createUser(userDto);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
 }
--- bcg-backend/src/main/java/net/bluecollargigs/bcgbackend/service/UserService.java
+++ bcg-backend/src/main/java/net/bluecollargigs/bcgbackend/service/UserService.java
@@ -1,5 +1,8 @@
 package net.bluecollargigs.bcgbackend.service;
 
+import net.bluecollargigs.bcgbackend.dto.UserDto;
+
 public interface UserService {
 
+    UserDto createUser(UserDto userDto);
 }

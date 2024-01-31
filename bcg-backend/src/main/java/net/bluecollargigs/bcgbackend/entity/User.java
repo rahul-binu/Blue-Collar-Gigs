46edf33f15ea5570438ce66e913ffff11cd9d33f
rahul-binu <just.commit.it@gmail.com>
2024-01-31 18:37:51 +0530

    Created user Entity

--- bcg-backend/src/main/java/net/bluecollargigs/bcgbackend/entity/User.java
+++ bcg-backend/src/main/java/net/bluecollargigs/bcgbackend/entity/User.java
@@ -1,5 +1,40 @@
 package net.bluecollargigs.bcgbackend.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user")
 public class User {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email_id", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "user_type", nullable = false)
+    private String userType;
+
+    @Column(name = "registration_date", nullable = false)
+    private LocalDateTime registrationDate;
 }

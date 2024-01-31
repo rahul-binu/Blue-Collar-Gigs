df659c991effa7ad16a6722ef45d869a58c0ab6b
rahul-binu <just.commit.it@gmail.com>
2024-01-31 18:41:48 +0530

    created the dto of the user entity

--- bcg-backend/src/main/java/net/bluecollargigs/bcgbackend/dto/UserDto.java
+++ bcg-backend/src/main/java/net/bluecollargigs/bcgbackend/dto/UserDto.java
@@ -1,5 +1,21 @@
 package net.bluecollargigs.bcgbackend.dto;
 
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
     
+    private Long id;
+    private String email;
+    private String password;
+    private String userType;
+    private LocalDateTime registrationDate;
 }

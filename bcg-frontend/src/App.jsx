b5b939e8f76822faca2c159579b5ec7b9cbe5039
rahul-binu <just.commit.it@gmail.com>
2024-02-05 18:18:20 +0530

    same day i went for the driving licence no big changes

--- bcg-frontend/src/App.jsx
+++ bcg-frontend/src/App.jsx
@@ -6,6 +6,8 @@ import Footer from './assets/components/Footer';
 import Home from './assets/components/Home';
 import Register from './assets/components/Register';
 import Login from './assets/components/Login';
+import Profile from './assets/components/Profile';
+import NewJobPost from './assets/components/NewJobPost';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,7 +22,10 @@ function App() {
           <Route path = '/Home' element = {<Home />}></Route>
           <Route path='/Register' element={<Register />} />
           <Route path = '/login' element = {<Login />}></Route>
+          <Route path='/profile' element={<Profile />}></Route>
+          <Route path='/postnewwork' element={<NewJobPost />}></Route>
         </Routes>
+        <Footer />
       </BrowserRouter>
 
     </>
--- bcg-frontend/src/assets/components/Home.jsx
+++ bcg-frontend/src/assets/components/Home.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
 
-function Home(){
-    return(
-        <div className="container bg-secondary">
-            <div className="row">
-                <div className="col">
-                <h3>Hello</h3>
-                </div>
-            </div>
+import "/src/assets/styles/Home.css";
+// import { Search } from "react-bootstrap-icons";
+
+
+function Home() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col text-center my-5">
+          <h1 id='siteTitle'>Empowering Skills, Fuelling Futures</h1>
         </div>
-    )
+      </div>
+
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <div className="pill-search-container">
+              <form className="d-flex">
+                <input className="form-control me-2 pill-search" type="search" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-outline-light" type="submit"></button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
 }
 
 export default Home;

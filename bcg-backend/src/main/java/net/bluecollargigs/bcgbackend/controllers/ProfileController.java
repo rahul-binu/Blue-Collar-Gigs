790fefda1eb93f32d38c4bf58e2846b4419484ee
rahul-binu <just.commit.it@gmail.com>
2024-02-13 18:43:30 +0530

    removed the unused dependencies from the profileController

--- bcg-backend/src/main/java/net/bluecollargigs/bcgbackend/controllers/ProfileController.java
+++ bcg-backend/src/main/java/net/bluecollargigs/bcgbackend/controllers/ProfileController.java
@@ -3,11 +3,8 @@ package net.bluecollargigs.bcgbackend.controllers;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.bluecollargigs.bcgbackend.dto.ProfileDto;
-import net.bluecollargigs.bcgbackend.dto.WorkerDto;
-import net.bluecollargigs.bcgbackend.mapper.ProfileMapper;
 import net.bluecollargigs.bcgbackend.service.ProfileService;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -17,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api")

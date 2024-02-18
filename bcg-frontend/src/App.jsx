69828fdb5eaf65cdc852b7263aae81c28375cefa
rahul-binu <just.commit.it@gmail.com>
2024-02-18 13:29:55 +0530

    added the work create to the routes

--- bcg-frontend/src/App.jsx
+++ bcg-frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Register from './assets/components/Register';
 import Login from './assets/components/Login';
 import Profile from './assets/components/Profile';
 import NewJobPost from './assets/components/NewJobPost';
+import WorkCreation from './assets/components/WorkCreation';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,12 +19,13 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path = '/' element = {<Home />}></Route>
-          <Route path = '/Home' element = {<Home />}></Route>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/Home' element={<Home />}></Route>
           <Route path='/Register' element={<Register />} />
-          <Route path = '/login' element = {<Login />}></Route>
+          <Route path='/login' element={<Login />}></Route>
           <Route path='/profile' element={<Profile />}></Route>
           <Route path='/postnewwork' element={<NewJobPost />}></Route>
+          <Route path='/WorkCreation' element={<WorkCreation />}></Route>
         </Routes>
         <Footer />
       </BrowserRouter>
